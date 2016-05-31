a01859ceb1024668428675544926acb70cafec11
allencwwong <allencwwong@gmail.com>
2016-05-31 01:11:10 -0700

    added new/create and index page

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,8 +1,24 @@
 class InventoriesController < ApplicationController
   def index 
+    @inventory = Inventory.all
   end
 
   def new
+    @inventory = Inventory.new
+  end
+
+  def create
+    @order_date = params[:order_date] 
+    @progress = params[:progress] 
+    @order_list = params[:order_list]
+    @due_date = params[:due_date]
+  
+    @inventory = Inventory.create(order_date: @order_date, progress: @progress, order_list: @order_list, due_date: @due_date)
+    if @inventory.save 
+    redirect_to "/"
+    else
+      render :new
+    end
   end
 
   def show
@@ -13,5 +29,5 @@ class InventoriesController < ApplicationController
 
   def destroy
   end
-  
+
 end
