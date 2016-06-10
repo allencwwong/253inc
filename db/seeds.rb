8e12cf8ea936a514ec74f1290d310f8eb60eb0ed
allencwwong <allencwwong@gmail.com>
2016-06-10 11:25:18 -0700

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Inventory.create!([
+  {order_date: "2015-01-01 00:00:00", progress: 10, order_list: "tk6", due_date: "2015-03-01 00:00:00", time_elasped: nil},
+  {order_date: "2015-10-01 00:00:00", progress: 30, order_list: "tk8", due_date: nil, time_elasped: nil}
+])
+OrderType.create!([
+  {order_type: "sales", amount: 1000},
+  {order_type: "quote", amount: 500},
+  {order_type: "sales", amount: 3000},
+  {order_type: "sales", amount: 1245},
+  {order_type: "quote", amount: 2000}
+])
+Sale.create!([
+  {date: "2016-06-09 00:00:00", by: "ACW", customer: "test 1", product_type: nil, due_date: nil, payment: nil, note: nil, order_type: "sales", amount: 3245},
+  {date: "2016-06-09 00:00:00", by: "ACW", customer: "test 2", product_type: nil, due_date: nil, payment: nil, note: nil, order_type: "quote", amount: 1288},
+  {date: "2016-06-08 00:00:00", by: "ACW", customer: "test 4", product_type: nil, due_date: nil, payment: nil, note: nil, order_type: "sales", amount: 1050},
+  {date: "2016-06-01 00:00:00", by: "ACW", customer: "test 5", product_type: nil, due_date: nil, payment: nil, note: nil, order_type: "sales", amount: 699},
+  {date: "2016-06-05 00:00:00", by: "ACW", customer: "test 6", product_type: nil, due_date: nil, payment: nil, note: nil, order_type: "sales", amount: 1299},
+  {date: "2016-06-04 00:00:00", by: "ACW", customer: "test 7", product_type: nil, due_date: nil, payment: nil, note: nil, order_type: "quote", amount: 2677},
+  {date: "2016-06-12 00:00:00", by: "ACW", customer: "test 8", product_type: nil, due_date: nil, payment: nil, note: nil, order_type: "quote", amount: 2030},
+  {date: "2016-06-10 00:00:00", by: "ACW", customer: "t1", product_type: nil, due_date: nil, payment: nil, note: nil, order_type: "sales", amount: 8000},
+  {date: "2016-06-10 00:00:00", by: "ACW", customer: "t2", product_type: nil, due_date: nil, payment: nil, note: nil, order_type: "quote", amount: 2300},
+  {date: "2016-05-10 00:00:00", by: "ACW", customer: "test 9", product_type: nil, due_date: nil, payment: nil, note: nil, order_type: "sales", amount: 3000}
+])
